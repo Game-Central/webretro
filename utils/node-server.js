ee76a866ab8cf86ec5f1b1fb4f3819baadcefead
Game-Central <101155240+Game-Central@users.noreply.github.com>
2022-08-24 11:20:59 -0700

    Update node-server.js

--- utils/node-server.js
+++ utils/node-server.js
@@ -9,7 +9,7 @@ app.use(express.static("..", {
 	setHeaders: (res) => {
 		res.set({
 			"Cross-Origin-Embedder-Policy": "require-corp",
-			"Cross-Origin-Opener-Policy": "same-origin",
+			"Cross-Origin-Opener-Policy": "unsafe-none",
 			"Access-Control-Allow-Origin": "*"
 		});
 	}
